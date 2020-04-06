ab0c79f542b08e3b0837790cd7270cb80fe25805
Leandro Rebes Camargo <lrebes@lnxcit021740.cit>
2020-04-06 15:52:40 -0300

    parcial parser e ast

--- 1s2020/uc_ast.py
+++ 1s2020/uc_ast.py
@@ -1,5 +1,5 @@
 class AST:
-    def __init__ (self):
+    def __init__(self):
         '''Ainda não sei oq colocar aqui...'''
 
     def show(buf=None, showcoord=True):
@@ -8,3 +8,365 @@ class AST:
     def ast(self):
         print("I'm on AST!")
 
+
+class ArrayDecl:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class ArrayRef:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class Assert:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class Assignment:
+    __slots__ = ('op', 'coord')
+
+    def __init__(self, op, coord=None):
+        self.op = op
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    attr_names = ('op',)
+
+
+class BinaryOp:
+    __slots__ = ('op', 'coord')
+
+    def __init__(self, op, coord=None):
+        self.op = op
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    attr_names = ('op',)
+
+
+class Break:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class Cast:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class Compound:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class Constant:
+    __slots__ = ('type', 'value', 'coord')
+
+    def __init__(self, type, value, coord=None):
+        self.type = type
+        self.value = value
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    attr_names = ('type', 'value',)
+
+
+class Decl:
+    __slots__ = ('name', 'coord')
+
+    def __init__(self, name, coord=None):
+        self.name = name
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    attr_names = ('name',)
+
+
+class DeclList:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class EmptyStatement:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class ExprList:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class For:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class FuncCall:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class FuncDecl:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class FuncDef:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class GlobalDecl:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class ID:
+    __slots__ = ('name', 'coord')
+
+    def __init__(self, name, coord=None):
+        self.name = name
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    attr_names = ('name',)
+
+
+class If:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class InitList:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class ParamList:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class Print:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class Program:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class PtrDecl:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class Read:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class Return:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class Type:
+    __slots__ = ('names', 'coord')
+
+    def __init__(self, names, coord=None):
+        self.names = names
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    attr_names = ('names',)
+
+
+class VarDecl:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+
+class UnaryOp:
+    __slots__ = ('op', 'coord')
+
+    def __init__(self, op, coord=None):
+        self.op = op
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
+
+    attr_names = ('op',)
+
+
+class While:
+    __slots__ = ('coord')
+
+    def __init__(self, coord=None):
+        self.coord = coord
+
+    def children(self):
+        nodelist = []
+        return tuple(nodelist)
--- 1s2020/uc_parser.py
+++ 1s2020/uc_parser.py
@@ -1,4 +1,4 @@
-from uc_ast import AST
+from uc_ast import *
 from uc_lexer import Lexer
 
 
@@ -20,3 +20,233 @@ class UCParser:
 
     def show(self, buf=None, showcoord=True):
         print("I'm on show")
+
+    def p_program(self, p):
+        """ program  : global_declaration_list
+        """
+        p[0] = Program(p[1])
+
+    def p_global_declaration_list(self, p):
+        """ global_declaration_list : global_declaration
+                                    | global_declaration_list global_declaration
+        """
+        p[0] = [p[1]] if len(p) == 2 else p[1] + [p[2]]
+
+    def p_identifier(self, p):
+        """ identifier : ID """
+        p[0] = ID(p[1], lineno=p.lineno(1))
+
+    def p_type_specifier(self, p):
+        """ type_specifier : void
+                           | char
+                           | int
+                           | float
+        """
+
+    def p_pointer(self, p):
+        """ pointer : pointer_opt
+        """
+
+    def p_pointer_opt(self, p):
+        """ pointer_opt : pointer
+                        | None
+        """
+
+    def p_declarator(self, p):
+        """ declarator : * pointer_opt direct_declarator
+        """
+
+    def p_direct_declarator(self, p):
+        """ direct_declarator : identifier
+                              | declarator
+                              | direct_declarator LBRACKET constant_expression_opt RBRACKET
+                              | direct_declarator LPAREN parameter_list RPAREN
+                              | direct_declarator LPAREN identifier RPAREN *
+        """
+
+    def p_constant_expression_opt(self, p):
+        """ constant_expression_opt : constant_expression
+                                    | None
+        """
+
+    def p_constant_expression(self, p):
+        """ constant_expression : binary_expression
+        """
+
+    def p_binary_expression(self, p):
+        """ binary_expression : cast_expression
+                              | binary_expression * binary_expression
+                              | binary_expression / binary_expression
+                              | binary_expression % binary_expression
+                              | binary_expression + binary_expression
+                              | binary_expression - binary_expression
+                              | binary_expression < binary_expression
+                              | binary_expression <= binary_expression
+                              | binary_expression > binary_expression
+                              | binary_expression >= binary_expression
+                              | binary_expression == binary_expression
+                              | binary_expression != binary_expression
+                              | binary_expression & & binary_expression
+                              | binary_expression | | binary_expression
+        """
+
+    def p_cast_expression(self, p):
+        """ cast_expression : unary_expression
+                            | LPAREN type_specifier RPAREN cast_expression
+        """
+
+    def p_unary_expression(self, p):
+        """ unary_expression : postfix_expression
+                             | ++ unary_expression
+                             | -- unary_expression
+                             | unary_operator cast_expression
+        """
+
+    def p_postfix_expression(self, p):
+        """ postfix_expression : primary_expression
+                               | postfix_expression LBRACKET expression RBRACKET
+                               | postfix_expression LPAREN argument_expression_opt RPAREN
+                               | postfix_expression ++
+                               | postfix_expression --
+        """
+
+    def p_argument_expression_opt(self, p):
+        """ argument_expression_opt : argument_expression
+                                    | None
+        """
+
+    def p_primary_expression(self, p):
+        """ primary_expression : identifier
+                               | constant
+                               | string
+                               | expression
+        """
+
+    def p_constant(self, p):
+        """ constant : integer_constant
+                     | character_constant
+                     | floating_constant
+        """
+
+    def p_expression(self, p):
+        """ expression : assignment_expression
+                       | expression COMMA assignment_expression
+        """
+
+    def p_argument_expression(self, p):
+        """ argument_expression : assignment_expression
+                                | argument_expression COMMA assignment_expression
+        """
+
+    def p_assignment_expression(self, p):
+        """ assignment_expression : binary_expression
+                                  | unary_expression assignment_operator assignment_expression
+        """
+
+    def p_assignment_operator(self, p):
+        """ assignment_operator : ==
+                                | *=
+                                | /=
+                                | %=
+                                | +=
+                                | -=
+        """
+
+    def p_unary_operator(self, p):
+        """ unary_operator : &
+                           | *
+                           | +
+                           | -
+                           | !
+        """
+
+    def p_parameter_list(self, p):
+        """ parameter_list : parameter_declaration
+                           | parameter_list COMMA parameter_declaration
+        """
+
+    def p_parameter_declaration(self, p):
+        """ parameter_declaration : type_specifier declarator
+        """
+
+    def p_declaration(self, p):
+        """ declaration : type_specifier init_declarator_list_opt SEMI
+        """
+
+    def p_init_declarator_list_opt(self, p):
+        """ init_declarator_list_opt : init_declarator_list
+                                     | None
+        """
+
+    def p_init_declarator_list(self, p):
+        """ init_declarator_list : init_declarator
+                                 | init_declarator_list COMMA init_declarator
+        """
+
+    def p_init_declarator(self, p):
+        """ init_declarator : declarator
+                            | declarator = initializer
+        """
+
+    def p_initializer(self, p):
+        """ initializer : assignment_expression
+                     | initializer_list
+                     | initializer_list COMMA
+        """
+
+    def p_initializer_list(self, p):
+        """ initializer_list : initializer
+                             | initializer_list COMMA initializer
+        """
+
+    def p_compound_statement(self, p):
+        """ compound_statement : declaration * statement *
+        """
+
+    def p_statement(self, p):
+        """ statement : expression_statement
+                      | compound_statement
+                      | selection_statement
+                      | iteration_statement
+                      | jump_statement
+                      | assert_statement
+                      | print_statement
+                      | read_statement
+        """
+
+    def p_expression_statement(self, p):
+        """ expression_statement : expression_opt SEMI
+        """
+
+    def p_expression_opt(self, p):
+        """ expression_opt : expression
+                           | None
+        """
+
+    def p_selection_statement(self, p):
+        """ selection_statement : IF expression statement
+                                | IF expression statement ELSE statement
+        """
+
+    def p_iteration_statement(self, p):
+        """ iteration_statement : WHILE LPAREN expression RPAREN statement
+                                | FOR LPAREN expression_opt SEMI expression_opt SEMI expression_opt RPAREN statement
+                                | FOR LPAREN declaration SEMI expression_opt SEMI expression_opt RPAREN statement
+        """
+
+    def p_jump_statement(self, p):
+        """ jump_statement : BREAK
+                           | RETURN expression_opt SEMI
+        """
+
+    def p_assert_statement(self, p):
+        """ assert_statement : ASSERT expression SEMI
+        """
+
+    def p_print_statement(self, p):
+        """ print_statement : PRINT LPAREN expression_opt RPAREN SEMI
+        """
+
+    def p_read_statement(self, p):
+        """ read_statement : READ LPAREN argument_expression RPAREN SEMI
+        """
